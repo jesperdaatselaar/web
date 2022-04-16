7de1816e03935276dbc2a52b21841b4a5fd769e6
Jesper van Daatselaar <jesperdaatselaar@gmail.com>
2022-04-17 01:12:29 +0200

    feat: :lipstick: Created ContactSection and Footer component

---
+++ src/components/ContactSection.js
@@ -0,0 +1,28 @@
+function Contact() {
+  return (
+    <section className="section">
+      <h1 className="title is-3">Get in touch</h1>
+      <h1 className="subtitle is-5">
+        To give feedback, get some pictures or create some together.
+      </h1>
+      <strong className="column">
+        <p>Email</p>
+        <a href="#">contact@jesperdaatselaar.com</a> <br />
+      </strong>
+      <div className="column">
+        <strong>
+          <p>Socials</p>
+        </strong>
+        <a href="">
+          <i class="fa-brands fa-instagram"></i> @jesperdaatselaar
+        </a>{" "}
+        <br />
+        <a href="">
+          <i class="fa-brands fa-twitter"></i> @jsprnl
+        </a>
+      </div>
+    </section>
+  );
+}
+
+export default Contact;
---
+++ src/components/Footer.js
@@ -0,0 +1,34 @@
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <section className="section columns">
+          <div className="column content">
+            <strong>Jesper van Daatselaar</strong> <br />
+            <p>A Dutch view on life.</p>
+            <p>Copyright &copy; 2022</p>
+          </div>
+          <div className="column">
+            <a href="">
+              <strong>About</strong>
+            </a>
+            <br />
+            <a href="">
+              <strong>Gear</strong>
+            </a>
+            <br />
+            <a href="">
+              <strong>Work</strong>
+            </a>
+            <br />
+            <a href="">
+              <strong>Contact</strong>
+            </a>
+          </div>
+        </section>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
