b4ffde4fafa246c1c10f44cd284748c8188a4037
Jesper van Daatselaar <jesperdaatselaar@gmail.com>
2022-04-17 01:13:04 +0200

    fix: :art: Changed format of TextSection

--- src/components/TextSection.js
+++ src/components/TextSection.js
@@ -1,13 +1,22 @@
-function TextSection (props) {
-    return (
-      <section className="section">
-        <div className="content">
-          <p>
-            {props.text}
-          </p>
-        </div>
-      </section>
-    );
+function TextSection() {
+  return (
+    <section className="section">
+      <div className="content">
+        <p>
+          <strong>The person behind the camera.</strong>,
+          <br />
+          `That will be me, Jesper! I'm a student from the Netherlands. I was
+          born in 2003, and since a young age I always loved to capture every
+          moment. But due to school, and cheap phones I never really captured
+          the pictures I wanted to. In January 2021 I switched everything
+          around, I bought my first camera: A Sony Alpha A6100. Since then I
+          have been capturing a lot of moments in different Dutch and some
+          European cities. Now, with the addition of a drone, a DJI Mini 2 to be
+          specific, I will capture even more and more.
+        </p>
+      </div>
+    </section>
+  );
 }
 
 export default TextSection;
